22293cd24dad57fdfddebc9fde5c8f39ce73de11
febirds <febirds52086@gmail.com>
2016-01-29 17:26:27 +0800

    m8

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,15 +5,27 @@ CREATE TABLE `admin_user` (
   `password` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(200) DEFAULT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `mcontent` (
   `id` varchar(64) NOT NULL,
   `user_name` varchar(45) DEFAULT NULL,
+  `projectId` int(11) DEFAULT NULL,
   `linkurl` varchar(200) DEFAULT NULL,
   `ctime` datetime DEFAULT NULL,
   `isUse` tinyint(4) DEFAULT '1',
   `auther` varchar(45) DEFAULT NULL,
   `title` varchar(200) DEFAULT NULL,
   `utime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `proName` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `users` (
@@ -28,3 +40,5 @@ CREATE TABLE `users` (
   `nick_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
